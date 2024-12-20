26c027022d2859a90f1c6a2bbf2c632bbe670028
Michael Cruz <mcruz@aersale.com>
2024-12-20 10:24:51 -0500

    counting strokes. exit case

--- main.go
+++ main.go
@@ -2,31 +2,13 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 type model struct {
-	choices  []string
-	cursor   int
-	selected map[int]struct{}
-}
-
-func main() {
-	p := tea.NewProgram(initialModel())
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
-		os.Exit(1)
-	}
-}
-
-func initialModel() model {
-	return model{
-		choices: []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
-
-		selected: make(map[int]struct{}),
-	}
+	count int
 }
 
 func (m model) Init() tea.Cmd {
@@ -35,56 +17,28 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-
 	case tea.KeyMsg:
-
 		switch msg.String() {
-
 		case "ctrl+c", "q":
 			return m, tea.Quit
-
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
-			}
-
-		case "enter", " ":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
-			}
+		default:
+			m.count++
+			return m, tea.Tick(time.Second, func(t time.Time) tea.Msg {
+				return nil
+			})
 		}
+	default:
+		return m, nil
 	}
-
-	return m, nil
 }
 
 func (m model) View() string {
-	s := "What should we buy at the market?\n\n"
+	return fmt.Sprintf("Count: %d\nPress any key to increment.", m.count)
+}
 
-	for i, choice := range m.choices {
-
-		cursor := " "
-		if m.cursor == i {
-			cursor = ">"
-		}
-
-		checked := " "
-		if _, ok := m.selected[i]; ok {
-			checked = "x"
-		}
-
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+func main() {
+	p := tea.NewProgram(model{})
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Error starting app: %v\n", err)
 	}
-
-	s += "\nPress q to quit.\n"
-
-	return s
 }
