6908c98d74d646b2e07ab763fac9ee7c06450ed7
Michael Cruz <mcruz@aersale.com>
2025-01-06 11:11:40 -0500

    added user queries

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,21 +1,19 @@
 -- name: CreateUser :one
-INSERT INTO
-    users (id, created_at, updated_at, email)
-VALUES
-    (
-        ?, -- UUID should be generated in application code and passed as a parameter
-        CURRENT_TIMESTAMP,
-        CURRENT_TIMESTAMP,
-        ?
-    ) RETURNING *;
+INSERT INTO users (id, first_name, last_name, email, created_at, updated_at)
+VALUES (
+    ?,  -- UUID should be generated in application code and passed as a parameter
+    ?,  -- first_name
+    ?,  -- last_name
+    ?,  -- email
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP
+)
+RETURNING *;
 
 -- name: GetUserByEmail :one
-SELECT
-    *
-FROM
-    users
-WHERE
-    email = ?;
+SELECT *
+FROM users
+WHERE email = ?;
 
 -- name: DeleteAllUsers :exec
 DELETE FROM users;
