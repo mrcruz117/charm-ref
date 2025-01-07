07bcfdd0ca21de585ce94e1741b9b64dae4a44f6
Michael Cruz <mcruz@aersale.com>
2025-01-07 11:55:43 -0500

    format change

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,12 +1,13 @@
 -- +goose Up
-CREATE TABLE users (
-    id TEXT PRIMARY KEY,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    created_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL
-);
+CREATE TABLE
+    users (
+        id TEXT PRIMARY KEY,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        created_at DATETIME NOT NULL,
+        updated_at DATETIME NOT NULL
+    );
 
 -- +goose Down
 DROP TABLE users;
